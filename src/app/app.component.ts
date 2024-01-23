beb3afb171c59e2143e05a60dfde2ff61107e0ed
ramanathan23 <c.ramanathan23@gmail.com>
2024-01-23 22:34:04 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { of, tap } from 'rxjs';
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularOptimization';
+  title = 'AngularToolsDemo';
+  constructor() {
+    new DatePipe('').transform('', '');
+    of().pipe(tap(c => c)).subscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+//import * as d from "d3";
+import * as axios from 'axios';
+import { scaleLinear } from 'd3-scale'
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(){
+    scaleLinear([]);
+  }
+}
