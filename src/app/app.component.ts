0e47683020e382a5336d8a0bf32443f76d56436c
ramanathan23 <c.ramanathan23@gmail.com>
2024-01-23 22:58:11 +0530

    updated source files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { of, tap } from 'rxjs';
 import { DatePipe } from '@angular/common';
+//import * as d from "d3";
+import * as axios from 'axios';
+import { scaleLinear } from 'd3-scale'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,5 +15,7 @@ export class AppComponent {
   constructor() {
     new DatePipe('').transform('', '');
     of().pipe(tap(c => c)).subscribe();
+    //d.scaleLinear([]);
+    scaleLinear([]);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-//import * as d from "d3";
-import * as axios from 'axios';
-import { scaleLinear } from 'd3-scale'
-
 
 @NgModule({
   declarations: [
@@ -20,6 +16,5 @@ import { scaleLinear } from 'd3-scale'
 })
 export class AppModule { 
   constructor(){
-    scaleLinear([]);
   }
 }
